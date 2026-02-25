177d0e5c4dfe3c526b16d7d55c9a096d42691940
Marc RUBIO MADRILEÑO <marc.rubio@alten.es>
2026-02-25 11:03:39 +0100

    Subida web en feo para testear github pages a posteriori

---
+++ script.js
@@ -0,0 +1,24 @@
+// ===== FECHA DE LA BODA =====
+const fechaBoda = new Date("2026-10-11T17:00:00").getTime();
+
+const contador = setInterval(function() {
+
+    const ahora = new Date().getTime();
+    const diferencia = fechaBoda - ahora;
+
+    const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+    document.getElementById("dias").innerHTML = dias;
+    document.getElementById("horas").innerHTML = horas;
+    document.getElementById("minutos").innerHTML = minutos;
+    document.getElementById("segundos").innerHTML = segundos;
+
+    if (diferencia < 0) {
+        clearInterval(contador);
+        document.getElementById("contador").innerHTML = "¡Ya llegó el gran día!";
+    }
+
+}, 1000);
